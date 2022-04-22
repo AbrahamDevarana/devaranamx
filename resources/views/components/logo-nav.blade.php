b4f0639635771df81b6f4ad8e9ba5ac89bf4d9f0
AbrahamAG <abrahamalvarado@devarana.mx>
2022-04-22 13:29:23 -0500

    Mejoras Optimización de imágenes y validación v6

--- resources/views/components/logo-nav.blade.php
+++ resources/views/components/logo-nav.blade.php
@@ -1,3 +1,3 @@
 
-<img src=" {{ asset('img/logos/Isotipo_blanco.png')}}" id="devaranaLogoBg" alt="Devarana"> 
+<img src=" {{ asset('img/logos/Isotipo_blanco.png')}}" id="devaranaLogoBg" alt="Devarana Icon" 
 {{ $attributes->merge(['class' => 'hidden md:flex m-auto duration-500 ease-in-out transition-all']) }}/>
